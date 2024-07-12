98cc54d290861a296a795969f43b7d9bd6ffee6a
jash.liao <jash.liao@gmail.com>
2024-07-12 16:28:16 +0800

    Create

---
+++ Code/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiApp1
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Detail { get; set; }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var items = new List<Item>
+            {
+                new Item { Name = "Item 1", Detail = "This is item 1" },
+                new Item { Name = "Item 2", Detail = "This is item 2" },
+                new Item { Name = "Item 3", Detail = "This is item 3" }
+            };
+
+            MyCollectionView.ItemsSource = items;
+        }
+    }
+
+}
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+﻿使用Copilot 詢問 C# MAUI 7.0 LISTVIEW 當項目被選擇 改變 背景顏色 和 文字顏色 [Create .NET MAUI ListView Example With Copilot]
+
+資料來源: Copilot
+
+直接問『C# MAUI 7.0 LISTVIEW 當項目被選擇 改變 背景顏色 和 文字顏色』 無法得到答案
+
+所以我把對話過程也備份到『Copilot Ans』
